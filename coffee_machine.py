f8ef75d54c36e89e1628448745f043b865e11e96
Michael Primavera <pastasauce59@gmail.com>
2023-02-26 23:26:55 -0500

    added 'menu' dictionary with coffees and ingredients needed, added 'resources' dictionary listing the coffee machines current amount of resources on hand.

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,1 +1,35 @@
 # 🚧 Coffee Machine Under Construction 🚧
+
+# TODO: Print a report of all the coffee machine resources.
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
