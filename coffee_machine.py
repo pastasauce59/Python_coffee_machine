24d71a4c708219556f09036d9120482637c46061
Michael Primavera <pastasauce59@gmail.com>
2023-02-28 20:10:36 -0500

    changed 'add_coins' function to return total only, ccreated 'make_drink' function to process users given money and if it is enough then to make and serve the drink.

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,5 +1,3 @@
-# 🚧 Coffee Machine Under Construction 🚧
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -63,23 +61,38 @@ def check_resources(current_resources, selected_drink):
 def add_coins(quarters, dimes, nickels, pennies):
     """Takes in coins, multiplies them by their respective value and then returns total money given by user."""
     total = (quarters * .25) + (dimes * .10) + (nickels * .05) + (pennies * .01)
-    return f"${total}"
+    return total
+
+
+def make_drink(money_given, choice_name, drink, resources_on_hand):
+    """Takes in money given by user and sees it if covers the cost of users desired drink, if so then updates coffee machine resources less ingredients needed for drink made and returns message."""
+    if money_given >= drink['cost']:
+        change = money_given - drink['cost']
+        print(f"Here is ${change} in change.")
+        
+        ingredients = drink['ingredients']
+        for key in ingredients:
+            resources_on_hand[key] = resources_on_hand[key] - ingredients[key]
+        resources_on_hand['money'] += drink['cost']
+        return f"Here is your {choice_name} ☕️. "
+    else:
+        return "Sorry, that is not enough. Money refunded."
 
 machine_on = True
 while machine_on:
     choice = input("What would you like? (espresso/latte/capuccino): ").lower()
-    if choice == 'espresso' or choice == 'latte' or choice == 'capuccino':
+    if choice == 'espresso' or choice == 'latte' or choice == 'cappuccino':
         sufficient_resources = check_resources(resources, MENU[choice])
         if sufficient_resources:
-            # print(f'Making an order of {choice}')
             print("Please insert coins.")
-            quarters = int(input("how many quarters?: "))
-            dimes = int(input("how many dimes?: "))
-            nickels = int(input("how many nickels?: "))
-            pennies = int(input("how many pennies?: "))
+            quarters = float(input("how many quarters?: "))
+            dimes = float(input("how many dimes?: "))
+            nickels = float(input("how many nickels?: "))
+            pennies = float(input("how many pennies?: "))
 
             money = add_coins(quarters, dimes, nickels, pennies)
-            print(money)
+            drink = make_drink(money, choice, MENU[choice], resources )
+            print(drink)
     elif choice == 'report':
         map = {
             'water': 'ml',
